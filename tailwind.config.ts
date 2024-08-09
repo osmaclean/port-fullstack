9cb38779a3959db944a3cd35d092d6657e0ef4da
Maclean <contatolucasmaclean@gmail.com>
2024-08-08 21:06:19 -0300

    feat: Added Header and Hero components in app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'primary-100': '#BFBFBD',
+        'primary-200': '#A6A6A6',
+        'primary-300': '#6F7372',
+        'primary-400': '#383E40',
+        'primary-500': '#222526',
+      },
+      fontFamily: {
+        heathergreen: ['Heathergreen', 'sans-serif'],
+        firacode: ['Fira Code', 'monospace'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
